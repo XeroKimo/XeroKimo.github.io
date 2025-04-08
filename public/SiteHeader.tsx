58b0f0cd84ed49afdf1b7429eeef1753cb958b64
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2025-04-08 17:15:51 -0400

    - Test adding a common site header

---
+++ public/SiteHeader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SiteHeader()
+{
+    return (
+        <>
+        <body>
+            <p><a href="index.html">Portfolio</a>  <a href="Blogs.html">Blogs</a></p>
+        </body>
+        </>
+    )
+}
+
+export default SiteHeader
