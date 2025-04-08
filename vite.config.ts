9d99636a9d9e89f684e841532e7cfde0aeea2c07
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2025-04-08 16:09:19 -0400

    - Testing multi-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'How_to_reason_about_exceptions.html'),
+      },
+    },
+  },
 })
