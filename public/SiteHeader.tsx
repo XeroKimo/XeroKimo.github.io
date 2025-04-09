a46c9f3435e890308dd397fb9036c9bec24dedbf
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2025-04-09 17:54:24 -0400

    - Moved the portfolio folder to the src folder - Messing around with the scripts and I think I got what I was looking for in regards to how to components

--- public/SiteHeader.tsx
+++ public/SiteHeader.tsx
@@ -5,7 +5,7 @@ function SiteHeader() : JSX.Element
     return (
         <>
         <body>
-            <p><a href="index.html">Portfolio</a>  <a href="Blogs.html">Blogs</a></p>
+            <p><a href="">Portfolio</a>  <a href="Blogs.html">Blogs</a></p>
         </body>
         </>
     )
